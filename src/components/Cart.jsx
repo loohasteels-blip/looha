4251780012ac05b6021859083294d1a45aaec910
loohasteels-blip <loohasteels@gmail.com>
2026-05-20 17:11:04 +0530

    fix: restore missing closing div in Cart.jsx - build error fix

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -112,8 +112,7 @@ export default function Cart() {
                             </div>
                         </div>
                     </div>
-                </div>
             </div>
-
+        </div>
     );
 }
